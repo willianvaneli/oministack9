73be2ae7e226aed0bd34863566fb1e65d0d80df5
willian bruschi vaneli <willianvaneli@gmail.com>
2020-02-05 14:01:23 -0300

    inicial

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-ud5hn.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+
+app.use(express.json())
+app.use(routes);
+
+
+app.listen(3333);
+
+
