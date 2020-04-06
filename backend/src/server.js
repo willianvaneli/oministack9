f4e0ddfaebb35f1b66a743999ddc8256500c9db2
willian bruschi vaneli <willianvaneli@gmail.com>
2020-04-06 10:41:15 -0300

    att

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -13,8 +16,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-ud5hn.mongodb.net
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
-
-app.use(express.json())
+app.use(cors());;
+app.use(express.json());
+app.use('/files',express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 
